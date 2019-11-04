598125d9062123497ff5328db454847a271af4d2
[Oluwabammydu] <[oluwabamise@gmail.com]>
2019-11-03 20:41:48 -0800

    Updating files

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, resp, ) => {
+    res.send("Hello API");
+});
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
